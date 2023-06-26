385a06b1f6df4603941059c10916a817c6c2192d
unknown <Maxel26>
2023-06-26 18:10:40 -0500

    Se realiza ultimo ejercicio con enunciado

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,3 +1,7 @@
+// Dibujar las siguentes lineas en un cuadrado que equivale a un área de 10 por 10 asteriscos. El resultado debe ser como el que se muestra a continuación de cada reto.
+
+// 2. El reto es dibujar todas las columnas impares.
+
 for (let i = 0; i < 10; i++) {
     let fila = ''
     for (let j = 0; j < 10; j++) {
